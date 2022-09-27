835d590f30c2a3413769405d57b0cd260241ca9b
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-09-27 18:42:17 +0700

    project commit

---
+++ src/components/Pages/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HeadPhone from '../assets/img/headphones.svg';
+import './css/Login.scss';
+import {Link} from "react-router-dom";
+
+class Login extends React.Component{
+    render() {
+        return(
+            <section id="main">
+                    
+                    <div className="main-row">
+                        <div className="main-row-img">
+                            <img className="head-phone-img" src={HeadPhone} alt=""/>
+                        </div>
+                        <div className="main-row-text">
+                            <h1>USic</h1>
+                            <p>Music for everyone</p>
+                            <Link to={"/home"} className="btn">
+                                Start Listening
+                            </Link>
+                        </div>
+                    </div>
+            </section>
+        );
+    }
+}
+
+export default Login;
